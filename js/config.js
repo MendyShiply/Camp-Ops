0c9f7539a457f6a33cda54ddd7553e287baa0cbb
MendyShiply <mendy@shiplyfulfillment.com>
2026-07-12 02:44:46 -0500

    Configure production Supabase

---
+++ js/config.js
@@ -0,0 +1,6 @@
+(function () {
+  window.CampOpsConfig = {
+    supabaseUrl: "https://mpjqosopenmtkzcqjffu.supabase.co",
+    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wanFvc29wZW5tdGt6Y3FqZmZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4Mjc3MDYsImV4cCI6MjA5OTQwMzcwNn0.03P2KQPiDa0vqBkCc2Q4SE7iLI_zCTcnIHiWXuq0Izs"
+  };
+})();
